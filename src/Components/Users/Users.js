efb39abd01a5cf456dfaade068d8cd17c578282c
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-10-23 05:45:09 +0600

    Display user from Db

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 
 const Users = () => {
+    const [users,setUsers] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/users')
+        .then(res => res.json())
+        .then(data => setUsers(data))
+    },[])
     return (
-        <Container><h2>This is users</h2></Container>
+        <Container>
+            <h1>Users</h1>
+            <ul>
+                {
+                    users.map(user => <li key={user._id}>Name : {user.name} Email : {user.email} 
+                                            <button>Update</button>
+                                            <button>X</button>
+                                      </li> )
+                }
+            </ul>
+        </Container>
     );
 };
 
